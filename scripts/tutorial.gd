196a765fba2cfcd2d03ca3d93fdff77215983234
NILBOGtheSavior <naztechmedia@gmail.com>
2025-06-18 11:41:58 -0500

    Slums environment initialized

--- scripts/tutorial.gd
+++ scripts/tutorial.gd
@@ -3,7 +3,16 @@ extends Node2D
 @onready var player = $Entities/HeroCat
 @onready var camera = $Camera
 
+var enemy_scene = preload("res://scenes/characters/assasin_cat.tscn")
+
 func _ready():
+	player.position = $Entities/HeroSpawn.position
 	camera.set_target(player)  # Tell the camera who to follow
 	
-	
+	# Spawn enemies
+	for marker in $Entities/EnemySpawnPoints.get_children():
+		
+		var enemy = enemy_scene.instantiate()
+		enemy.global_position = marker.global_position
+		enemy.player = player
+		add_child(enemy)
