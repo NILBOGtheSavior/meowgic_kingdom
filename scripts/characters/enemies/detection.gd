8601cdd5b93dc848e25817dc8b3f22f07adf7a61
NILBOGtheSavior <naztechmedia@gmail.com>
2025-06-20 01:18:59 -0500

    Fixed z-index layering

--- scripts/characters/enemies/detection.gd
+++ scripts/characters/enemies/detection.gd
@@ -6,7 +6,6 @@ func _ready():
 func _on_body_entered(body):
 	if not body.is_in_group("player"):
 		return  # ignore non-player bodies
-
 	print("Player entered range:", body.name)
 	# your combat/event logic here
 	
